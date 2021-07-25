a917c31af8b48489bdb7118739697653c0a72f88
Tasheem Hargrove <tasheem@tasheems-mbp.home>
2021-07-25 17:37:23 -0500

    Create separate navbar for logged in users

--- nav/render-navbar.js
+++ nav/render-navbar.js
@@ -4,17 +4,17 @@ if(token != null) {
     let div = document.getElementById('render-navbar');
     
     let request = new XMLHttpRequest();
-    request.open('GET', 'nav/navbar-logged-in.html', true);
+    request.open('GET', '../nav/navbar-logged-in.html', true);
     
     request.onload = function() {
         if(this.status == 200 || this.status == 304) {
             let content = this.responseText;
             div.innerHTML = content;
 
-            let name = window.sessionStorage.getItem('Name');
+            /* let name = window.sessionStorage.getItem('Name');
             
             let greeting = document.getElementById('Greeting');
-            greeting.textContent = 'Welcome, ' + name + '!';
+            greeting.textContent = 'Welcome, ' + name + '!'; */
 
             document.getElementById('logout').addEventListener(
                 'click', logout
@@ -44,11 +44,11 @@ if(token != null) {
 }
 
 function login() {
-    window.location.href = 'http://127.0.0.1:5500/views/login.html';
+    window.location.href = 'http://127.0.0.1:5500/views/index.html';
 }
 
 function logout() {
     window.sessionStorage.removeItem('Token');
-    window.sessionStorage.removeItem('Name');
+    // window.sessionStorage.removeItem('Name');
     window.location.href = 'http://127.0.0.1:5500/views/index.html';
 }
