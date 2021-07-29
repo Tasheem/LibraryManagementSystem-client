48983c706bf5412ef94fa8e82cacb005fa1fd948
Tasheem Hargrove <tasheem@tasheems-mbp.home>
2021-07-28 20:24:38 -0500

    Adjust payload and apply user feedback

--- scripts/createBook.js
+++ scripts/createBook.js
@@ -6,12 +6,13 @@ function createBook() {
     let name = document.getElementById('name').value;
     let authorFName = document.getElementById('authorFirstName').value;
     let authorLName = document.getElementById('authorLastName').value;
+    let price = document.getElementById('price').value;
 
     let payload = {
         "name": `${name}`,
         "authorFirstName": `${authorFName}`,
         "authorLastName": `${authorLName}`,
-        "price": `${price}`
+        "price": parseFloat(price)
     }
 
     let request = new XMLHttpRequest();
@@ -23,16 +24,24 @@ function createBook() {
     request.setRequestHeader('Authorization', 'Bearer ' + token);
     request.setRequestHeader('Content-Type', 'application/json');
 
+    let feedback = document.getElementById('feedback');
     request.onload = function() {
         if(this.status === 201) {
-            console.log('RESPONSE: ');
-            console.log(request.response);
+            console.log('CREATED');
+            feedback.textContent = 'Book Created!';
+            feedback.style.color = '#21bf30';
+            setTimeout(() => {
+                window.location.href = 'http://127.0.0.1:5500/views/index.html';
+            }, 2500);
         }
     }
 
     request.onerror = () => {
+        feedback.textContent = 'Error Creating Book!';
+        feedback.style.color = 'red';
         console.log('error');
     }
 
-    request.send(payload);
+    let json = JSON.stringify(payload);
+    request.send(json);
 }
